1655d74cd62408b9dee17f78d47585f27a96c7e0
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-04-01 11:15:55 -0700

    fix: dependency

--- commit-cli.js
+++ commit-cli.js
@@ -7,6 +7,6 @@ const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
 bootstrap({
   cliPath: path.dirname(require.resolve('commitizen/package.json')),
   config: {
-    "path": "@commitlint/prompt"
+    "path": "cz-conventional-changelog"
   }
 });
